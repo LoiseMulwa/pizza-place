edab6dd87db081290acee5f982b9dc055e5f6edb
LoiseMulwa <loisemulwa7@gmail.com>
2022-03-20 23:27:11 +0300

    ft: declared variables in js

---
+++ script.js
@@ -0,0 +1,18 @@
+$("#btn").click(function() {
+    let pizzaSize=$(".round1 option:selected").value();
+    let pizzaTopping=$(".round2 option:selected").value();
+    let pizzaCrust=$(".round3 option:selected").value();
+    let total = parseInt(pizzaSize) + parseInt(pizzaTopping) + parseInt(pizzaCrust);
+    let order = 1;
+    let Amount = 0;
+
+});
+
+
+
+
+
+
+
+
+
